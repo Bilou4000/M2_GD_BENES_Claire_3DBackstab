4eaeb2fd9e52b27559b1526e96daa099d2f12c21
Bilou4000 <claire.benes@artfx.fr>
2024-01-24 10:47:44 +0100

    [initial commit]

---
+++ Assets/Script/CharacterMovement.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public static CharacterMovement instance;
+
+    [SerializeField] GameObject canBeSeenText, canBackStabText;
+    [SerializeField] private bool canStab, stabing;
+    [SerializeField] private float speed = 5f, stabDistance;
+    [SerializeField] private float rotationSpeed = 15f;
+    [SerializeField] Animator animator;
+    private GameObject theEnemy;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+        //rotation
+        float directionY = Input.GetAxis("Horizontal");
+        transform.Rotate(0, rotationSpeed * directionY * Time.deltaTime, 0);
+
+        //movement
+        float directionZ = Input.GetAxis("Vertical");
+        Vector3 movement = directionZ * transform.forward * speed * Time.deltaTime;
+        transform.position = transform.position + movement;
+
+
+        GameObject[] allEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+        for (int i = 0; i < allEnemies.Length; i++)
+        {
+            if (allEnemies[i].GetComponent<EnemyScript>().Vision() == true)
+            {
+                canStab = false;
+                canBeSeenText.SetActive(true);
+                canBackStabText.SetActive(false);
+                return;
+            }
+            else
+            {
+                canStab = true;
+                canBeSeenText.SetActive(false);
+            }
+
+            if(theEnemy != null)
+            {
+                stabDistance = theEnemy.GetComponent<EnemyScript>().StabDistance();
+
+                if (Vector3.Distance(theEnemy.transform.position, transform.position) > stabDistance)
+                {
+                    stabing = false;
+                    canBackStabText.SetActive(false);
+                }
+
+                if(Vector3.Dot(transform.forward, theEnemy.transform.forward) < 0)
+                {
+                    stabing = false;
+                    canBackStabText.SetActive(false);
+                }
+            }
+
+        }
+
+        if (stabing)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                animator.SetTrigger("attack");
+                theEnemy.GetComponent<EnemyScript>().Health(1f);
+                stabing = false;
+            }
+        }
+    }
+
+    public void Stab(GameObject enemy)
+    {
+        if(canStab)
+        {
+            stabing = true;
+            theEnemy = enemy;
+            canBackStabText.SetActive(true);
+        }
+        else
+        {
+            stabing = false;
+            canBackStabText.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Script/EnemyScript.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] GameObject text;
+    [SerializeField] float maxStabDistance, maxVisionDistance, life = 3;
+    private bool canStab, canBeSeen;
+
+    private void Update()
+    {
+        float Distance = Vector3.Distance(player.position, transform.position);
+
+        if (Vector3.Dot(transform.forward, player.position - transform.position) > 0)
+        {
+            if(Distance < maxVisionDistance)
+            {
+                canBeSeen = true;
+            }
+            else
+            {
+                canBeSeen= false;
+            }
+        }
+        else
+        {
+            canBeSeen = false;
+        }
+
+        if(Vector3.Dot(transform.forward, player.position - transform.position) < 0)
+        {
+            if (Vector3.Dot(transform.forward, player.forward) > 0)
+            {
+                if (Distance < maxStabDistance)
+                {
+                    CharacterMovement.instance.Stab(gameObject);
+                }
+            }
+        }
+    }
+
+    public bool Vision()
+    {
+        return canBeSeen;
+    }
+
+    public void Health(float health)
+    {
+        life -= health;
+        Debug.Log(life);
+    }
+
+    public float StabDistance()
+    {
+        return maxStabDistance;
+    }
+
+    //public float StabDistance()
+    //{
+    //    return maxStabDistance;
+    //}
+}
